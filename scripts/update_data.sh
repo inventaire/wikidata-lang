d8da899f52a162fb4a92ddc93926bf3b8fa306da
maxlath <k@maxlath.eu>
2021-05-21 11:21:35 +0200

    update_data: pin wikibase-cli version

--- scripts/update_data.sh
+++ scripts/update_data.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-npx --package=wikibase-cli wd sparql ./scripts/languages_data.rq > ./scripts/languages_data_query_results.json
+npx --package=wikibase-cli@15 wd sparql ./scripts/languages_data.rq > ./scripts/languages_data_query_results.json
