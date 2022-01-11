f141336b119dc7209cede2247847f88bb08cb9df
maxlath <k@maxlath.eu>
2022-01-11 16:46:01 +0100

    update tests
    addressing https://github.com/inventaire/wikidata-lang/issues/2

--- test/indexes.js
+++ test/indexes.js
@@ -1,19 +1,19 @@
 require('should')
 
-const byWdId = require('../by_wd_id')
-const byCode = require('../by_code')
+const byWdId = require('../indexes/by_wd_id')
+const byWmCode = require('../indexes/by_wm_code')
 
 describe('indexes', () => {
   it('should match', () => {
-    for (const code in byCode) {
-      for (const langData of byCode[code]) {
+    for (const code in byWmCode) {
+      for (const langData of byWmCode[code]) {
         langData.should.equal(byWdId[langData.wd])
       }
     }
   })
 
   it('should return the same object from both indexes', () => {
-    const a = byCode.uk[0]
+    const a = byWmCode.uk[0]
     const b = byWdId.Q8798
     a.should.equal(b)
   })
